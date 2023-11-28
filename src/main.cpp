327c58728696540e3adfe4dbd539c51f16c8a260
Siborg18 <github@syborg.co.uk>
2023-11-28 08:44:42 +0000

    Font loading now working directory correct.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
 #include <SFML/Graphics.hpp>
 
+
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    window.create(sf::VideoMode(1024, 768, desktop.bitsPerPixel), "SFML window");
+    //sf::RenderWindow window(sf::VideoMode(200, 200));
+    sf::CircleShape shape(100.f);
+    sf::Font font;
+    font.loadFromFile("src/NimbusMonoPS-Regular.otf");
+    sf::Text someText("Some test text", font, 15U);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        sf::Event event;
+        while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
+            window.close();
         }
 
         window.clear();
+        window.draw(shape);
+        window.draw(someText);
         window.display();
     }
+
+    return 0;
 }
