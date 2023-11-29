88af38f6dd28ffa66a84bc7481a1c2d387578fab
Siborg18 <github@syborg.co.uk>
2023-11-29 12:18:06 +0000

    Crashing Debian when drawing.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,35 @@
+#include <iostream>
+#include <limits.h>
 #include <SFML/Graphics.hpp>
+#include <string>
+#include <unistd.h>
 
+std::string getexepath()
+{
+  char result[ PATH_MAX ];
+  ssize_t count = readlink( "/proc/self/exe", result, PATH_MAX );
+  return std::string( result, (count > 0) ? count : 0 );
+}
+
+sf::Font loadmyFont()
+{
+    sf::Font font;
+    if (!font.loadFromFile("NimbusMonoPS-Regular.otf"))
+    {
+        std::cout << "Could not load font." << std::endl;
+    }
+    return font;
+}
 
 int main()
 {
+    std::cout << getexepath();
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
     window.create(sf::VideoMode(1024, 768, desktop.bitsPerPixel), "SFML window");
     //sf::RenderWindow window(sf::VideoMode(200, 200));
     sf::CircleShape shape(100.f);
-    sf::Font font;
-    font.loadFromFile("src/NimbusMonoPS-Regular.otf");
+    sf::Font font = loadmyFont();
     sf::Text someText("Some test text", font, 15U);
     shape.setFillColor(sf::Color::Green);
 
