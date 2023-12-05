4c1ce397ac4ae11fd3a8168dab1b8c07a2a1049d
Siborg18 <github@syborg.co.uk>
2023-12-05 10:10:41 +0000

    Intersect not working as intended

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 #include <unistd.h>
+#include "shapes.h"
+
 
 std::string getexepath()
 {
@@ -60,20 +62,44 @@ bool locationValid(sf::Text &text)
 return true;
 }
 
+void printCurrentLocation(sf::Shape &shape)
+{
+    sf::Vector2 myLocation = shape.getPosition();
+    std::cout << "x: " << myLocation.x << " y: " << myLocation.y << std::endl;
+}
 
 
 int main()
-{
+{ 
     std::cout << getexepath();
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
     window.create(sf::VideoMode(1024, 768, desktop.bitsPerPixel), "SFML window");
     //sf::RenderWindow window(sf::VideoMode(200, 200));
-    sf::CircleShape shape(100.f);
+    sf::CircleShape myCircleShape(10.f);
+    myCircleShape.setFillColor(sf::Color::Green);
+    sf::RectangleShape myRectangleShape;
+    myRectangleShape.setFillColor(sf::Color::Green);
+    myRectangleShape.setSize(sf::Vector2f(10, 5));
+    sf::RectangleShape anotherRectangleShape;
+    anotherRectangleShape.setFillColor(sf::Color::Blue);
+    anotherRectangleShape.setSize(sf::Vector2f(15.f, 15.f));
     sf::Font font = loadmyFont();
-    sf::Text someText("Some test text", font, 15U);
-    shape.setFillColor(sf::Color::Green);
-
+    sf::Text someText("", font, 20U);
+    sf::Text someMoreText("", font, 20U);
+    if(locationValid(someText))
+    {
+        someText.setPosition(0.f, 20.f);
+    }
+    if (locationValid(anotherRectangleShape))
+    {
+        anotherRectangleShape.setPosition(20.f, 600 / 2.f);
+    }
+    if (locationValid(myRectangleShape))
+    {   
+        myRectangleShape.setPosition(0, 0);
+    }
+    
     while (window.isOpen())
     {
         sf::Event event;
@@ -82,17 +108,35 @@ int main()
             if (event.type == sf::Event::Closed)
             window.close();
         }
-
         window.clear();
-        window.draw(shape);
+        //window.draw(myCircleShape);
+        window.draw(myRectangleShape);
+        window.draw(anotherRectangleShape);
+        sf::FloatRect boundingAnotherBox = anotherRectangleShape.getGlobalBounds();
+        sf::FloatRect boundingRectangleBox = myRectangleShape.getGlobalBounds();
+        // printCurrentLocation(myRectangleShape);
+        // std::cout << (boundingAnotherBox.intersects(boundingRectangleBox));
         window.draw(someText);
-        if (locationValid(shape))
+        window.draw(someMoreText);
+        if (boundingAnotherBox.intersects(boundingRectangleBox) && myRectangleShape.getPosition() != sf::Vector2f(0, 0));
         {
-            shape.move(1.5 , 1.5);  
+            sf::Vector2f rectanglePosition {myRectangleShape.getPosition()};
+            sf::Vector2f anotherRectanglePosition {anotherRectangleShape.getPosition()};
+
+            int x1 = rectanglePosition.x;
+            int y1 = rectanglePosition.y;
+            int x2 = anotherRectanglePosition.x;
+            int y2 = anotherRectanglePosition.y;
+            std::string someString = "Intersected at: x: " + std::to_string(x1) + " y: " + std::to_string(y1);
+            std::string someOtherString = "Intersected at: x: " + std::to_string(x2) + " y: " + std::to_string(y2);
+            someText.setString(someString);
+            someMoreText.setString(someOtherString);
+            anotherRectangleShape.setFillColor(sf::Color::Red);
         }
-        if (locationValid(someText))
+
+        if (locationValid(myRectangleShape))
         {
-            someText.move(1.f, 1.f);
+            myRectangleShape.move(0.f, 0.1);
         }
         window.display();
         
---
+++ src/shapes.cpp
@@ -0,0 +1,8 @@
+class shape
+{
+    public:
+    int do_something()
+    {
+        return 0;
+    }
+};
